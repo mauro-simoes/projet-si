4b65eec5d994d861f4d492b8ed8e1e0db4710a30
LeanAmira <amiratnael@gmail.com>
2024-05-11 21:27:32 +0200

    Page d'accueil 'fait'

---
+++ front/src/app/pages/Categorie.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardTitle,
+  CardHeader,
+  CardFooter
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+
+const categories = [
+  { title: "Caméra", img: '../../assets/img/camera.jpg'},
+  { title: "Appareil Photo", img: '../../assets/appareil-photo.jpg'},
+  { title: "Microphone", img: '../../assets/microphone.jpg'},
+  { title: "Pied d'éclairage", img: '../../assets/pied-eclairage.jpg'},
+  { title: "Carte mémoire", img: '../../assets/carte-memoire.jpg'},
+  { title: "Moniteurs", img: '../../assets/moniteur.jpg'},
+  { title: "Streamers", img: '../../assets/streamer.jpg'}
+];
+
+export default function Accueil() {
+  return (
+    <div className="home-page h-full">
+      <h1 className="text-3xl font-bold underline text-center my-5">Découvrez nos catégories</h1>
+      <div className="flex flex-wrap justify-center gap-15 p-10">
+        {categories.map((category) => (
+          <div key={category.title} className="w-60 p-5">
+            <Card className="h-50">
+              <CardHeader>
+                <CardTitle>{category.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                  <img src={category.img} className="w-full h-auto" alt="img non trouvé"/>
+              </CardContent>
+              <CardFooter>
+                <Button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+                  Découvrir..
+                </Button>
+              </CardFooter>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
