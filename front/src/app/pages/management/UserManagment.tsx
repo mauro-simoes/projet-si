638713d42dd48d2a71f63bccf181fc133b5d442d
Mauro Simoes <maurosimoes9@gmail.com>
2024-05-15 20:28:48 +0200

    user management connected

--- front/src/app/pages/management/UserManagment.tsx
+++ front/src/app/pages/management/UserManagment.tsx
@@ -36,77 +36,100 @@ import {
 import { approveUserAccount, changeUserPasswordAsAdmin, getAllUsers, suspendUserAccount } from '../../services/user/UserService'
 import { AdminPasswordChangeRequest, User } from '../../models/user/UserModels'
 import { Button } from '@/components/ui/button'
-import { ACCOUNT_STATUSES, RADIE } from '../../core/constants'
+import { ACCOUNT_STATUSES, RADIE, TOKEN, VALIDE } from '../../core/constants'
 import { toast } from 'sonner'
 import { Input} from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function UserManagement() {
   const [motDePasse, setMotDePasse] = useState("");
 
-  const [token,setToken] = useState("");
+  const navigator = useNavigate();
+
   const [users,setUsers] = useState([{} as User]);
   const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
 
-  function approveAccount(userId :number){
-    approveUserAccount(userId, token).
-    then(data => {
-      if(data){
-        toast.success("Profil validé avec succes");
-      }else{
-        toast.error("Echec de la validation");
-      }
-    }).
-    catch(error => {
-      toast.error("Echec de la validation", error);
-    });
-    loadData();
+  function approveAccount(id :number){
+    let tokenLocalStorage = localStorage.getItem(TOKEN);
+    if(tokenLocalStorage == null){
+      navigator("/acceuil",{replace:true});
+    }else{
+      approveUserAccount(id, tokenLocalStorage).
+      then(data => {
+        if(data){
+          toast.success("Profil validé avec succes");
+        }else{
+          toast.error("Echec de la validation");
+        }
+      }).
+      catch(error => {
+        toast.error("Echec de la validation" + error.response.data.message);
+      });
+      loadData(tokenLocalStorage);
+    }
   }
 
-  function suspendAccount(userId :number){
-    suspendUserAccount(userId, token).
-    then(data => {
-      if(data){
-        toast.success("Profil radié avec succes");
-      }else{
-        toast.error("Echec de la radiation");
-      }
-    }).
-    catch(error => {
-      toast.error("Echec de la radiation", error);
-    });
-    loadData();
+  function suspendAccount(id :number){
+    let tokenLocalStorage = localStorage.getItem(TOKEN);
+    if(tokenLocalStorage == null){
+      navigator("/acceuil",{replace:true});
+    }else{
+      suspendUserAccount(id, tokenLocalStorage).
+      then(data => {
+        if(data){
+          toast.success("Profil radié avec succes");
+        }else{
+          toast.error("Echec de la radiation");
+        }
+      }).
+      catch(error => {
+        toast.error("Echec de la radiation" + error.response.data.message);
+      });
+      loadData(tokenLocalStorage);
+    }
   }
 
-  function changePassword(userId :number, motDePasse: string){
-    changeUserPasswordAsAdmin({userId, motDePasse} as AdminPasswordChangeRequest, token).
-    then(data => {
-      if(data){
-        toast.success("Mot de passe de l'utilisateur " + userId + " changé avec succes");
-      }else{
-        toast.error("Echec de la mise à jour");
-      }
-    }).
-    catch(error => {
-      toast.error("Echec de la mise à jour", error);
-    });
-    setMotDePasse("");
+  function changePassword(id :number, newPassword: string){
+    let tokenLocalStorage = localStorage.getItem(TOKEN);
+    if(tokenLocalStorage == null){
+      navigator("/acceuil",{replace:true});
+    }else{
+      changeUserPasswordAsAdmin({id, newPassword} as AdminPasswordChangeRequest, tokenLocalStorage).
+      then(data => {
+        if(data){
+          toast.success("Mot de passe de l'utilisateur " + id + " changé avec succes");
+        }else{
+          toast.error("Echec de la mise à jour");
+        }
+      }).
+      catch(error => {
+        toast.error("Echec de la mise à jour" + error.response.data.message);
+      });
+      setMotDePasse("");
+      setIsEditDialogOpen(false);
+    }
   }
 
-  function loadData(){
+  function loadData(token:string){
     getAllUsers(token)
-      .then(data => {
-          setUsers(data);
-      })
+      .then(response => {
+          if(response.data != null){
+            setUsers(response.data);
+          }
+      }).catch(error => {
+        toast.error("Les utilisateurs n'ont pas pu etre recuperes : " + error.response.data.message);
+     });
   }
 
   useEffect(() => {
-      let tokenLocalStorage = localStorage.getItem("token");
+      let tokenLocalStorage = localStorage.getItem(TOKEN);
       if(tokenLocalStorage == null){
-          setToken("token");
+        navigator("/acceuil",{replace:true});
+      }else{
+        loadData(tokenLocalStorage);
       }
-      loadData();
   },[]);
 
   return (
@@ -126,23 +149,20 @@ export default function UserManagement() {
               </TableHeader>
               <TableBody>
                 {users.map((user) => (
-                  <TableRow key={user.userId}>
+                  <TableRow key={user.id}>
                       <TableCell>
-                        <div className="font-medium">{user?.nom?.toUpperCase()} {user.prenom}</div>
+                        <div className="font-medium">{user?.lastName?.toUpperCase()} {user.firstName}</div>
                         <div className="hidden text-sm text-muted-foreground md:inline">
-                          {user.email}
+                          {user.mail}
                         </div>
                       </TableCell>
                       <TableCell className="hidden sm:table-cell">
-                        <Badge className="text-xs" variant={user.status == RADIE ? "destructive" : "outline"}>
-                          {ACCOUNT_STATUSES[user.status]}
+                        <Badge className="text-xs" variant={user.accountStatus == RADIE ? "destructive" : "outline"}>
+                          {ACCOUNT_STATUSES[user.accountStatus]}
                         </Badge>
                       </TableCell>
-                      <TableCell className="text-center">
-                        <Rating
-                          style={{ maxWidth: 100 }}
-                          value={user.note}
-                          readOnly />
+                      <TableCell>
+                        {user.note == null ? "Non noté" : user.note /10}
                       </TableCell>
                       <TableCell>
                         <DropdownMenu>
@@ -154,10 +174,10 @@ export default function UserManagement() {
                           </DropdownMenuTrigger>
                           <DropdownMenuContent align="end">
                             <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                            {user.status == RADIE ?
-                              <DropdownMenuItem className='cursor-pointer' onClick={() => approveAccount(user.userId)}>Valider</DropdownMenuItem>
+                            {user.accountStatus == RADIE ?
+                              <DropdownMenuItem className='cursor-pointer' onClick={() => approveAccount(user.id)}>Valider</DropdownMenuItem>
                               :
-                              <DropdownMenuItem className='cursor-pointer' onClick={() => suspendAccount(user.userId)}>Radier</DropdownMenuItem>}
+                              <DropdownMenuItem className='cursor-pointer' onClick={() => suspendAccount(user.id)}>Radier</DropdownMenuItem>}
                             <DropdownMenuItem className='cursor-pointer' onClick={() => setIsEditDialogOpen(true)}>Modifier mot de passe</DropdownMenuItem>
                           </DropdownMenuContent>
                         </DropdownMenu>
@@ -166,7 +186,7 @@ export default function UserManagement() {
                             <DialogHeader>
                               <DialogTitle>Modifier mot de passe</DialogTitle>
                               <DialogDescription>
-                                Utilisateur : {user?.nom?.toUpperCase()} {user.prenom} - {user.email}
+                                Utilisateur : {user?.lastName?.toUpperCase()} {user.firstName} - {user.mail}
                               </DialogDescription>
                             </DialogHeader>
                               <div className="items-center ">
@@ -174,7 +194,7 @@ export default function UserManagement() {
                                 <Input className="col-span-3" onChange={(e) => {setMotDePasse(e.target.value)}}/>
                               </div>
                             <DialogFooter>
-                              <Button onClick={() => changePassword(user.userId, motDePasse)}>Sauvegarder</Button>
+                              <Button onClick={() => changePassword(user.id, motDePasse)}>Sauvegarder</Button>
                             </DialogFooter>
                           </DialogContent>
                         </Dialog>
